09df7188cb2bee6ac2c76b6bed01c65162bd1619
Rafael Martins <rmartins858@gmail.com>
2022-11-09 16:04:55 -0300

    fix: desafio2 correção 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,8 +1,13 @@
 // 2 - Ordene a coleção produtos pela quantidade de lanches vendidos em ordem crescente, mostrando apenas o nome e a quantidade de lanches vendidos
-db.produtos.find({}, { 
-    nome: 1,
+db.produtos
+  .find(
+    {},
+    {
+      nome: 1,
+      vendidos: 1,
+      _id: false,
+    }
+  )
+  .sort({
     vendidos: 1,
-    _id: false
-}).sort({
-    vendidos:1
-});
+  });
