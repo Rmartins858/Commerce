5664900aa36fba928e1a858bfe624c79bf478fb1
Rafael Martins <rmartins858@gmail.com>
2022-11-09 16:56:24 -0300

    fix: desafio5

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,4 @@
+//4 - Retorne os lanches que tiveram vendas maiores que 50 e menores que 100, mostrando apenas o nome e a quantidade de lanches vendidos em ordem crescente
 db.produtos
   .find(
     {
