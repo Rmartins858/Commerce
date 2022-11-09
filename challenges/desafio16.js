3a6e9c8577d26ceb27243582fd887866cc06959d
Rafael Martins <rmartins858@gmail.com>
2022-11-09 20:09:20 -0300

    fix: desafio16

---
+++ challenges/desafio16.js
@@ -0,0 +1,26 @@
+db.produtos.updateMany(
+    {
+      nome: {
+        $eq: "Big Mac",
+      },
+    },
+    {
+      $set: {
+        ultimaModificacao: {
+          $type: "timestamp",
+        },
+      },
+    },
+  );
+  
+  db.produtos.find(
+    {
+      ultimaModificacao: { $exists: true },
+    },
+  
+    {
+      _id: false,
+      nome: 1,
+    }
+  );
+  
