cf37baa6014dcdcea61d75104a8f3681d7ac8c08
Rafael Martins <rmartins858@gmail.com>
2022-11-09 17:01:20 -0300

    fix: desafio5 coreção

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,4 +1,3 @@
-//4 - Retorne os lanches que tiveram vendas maiores que 50 e menores que 100, mostrando apenas o nome e a quantidade de lanches vendidos em ordem crescente
 db.produtos
   .find(
     {
--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,4 +1,3 @@
-//5 - Retorne o nome, as curtidas e vendidos dos lanches que tiveram quantidade de curtidas igual a 36 ou tenham a quantidade de vendas igual a 85
 db.produtos.find(
   {
     $or: [{ curtidas: 36 }, { vendidos: 85 }],
@@ -8,5 +7,5 @@ db.produtos.find(
     curtidas: 1,
     vendidos: 1,
     _id: false,
-  }
+  },
 );
